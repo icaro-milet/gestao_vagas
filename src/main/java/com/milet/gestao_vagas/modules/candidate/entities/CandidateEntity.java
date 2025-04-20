b3fc86d1d63ac5003e12e69d43b56b4d7e0a8876
milet <milet@milets-Mac-mini.local>
2025-04-20 16:49:15 -0300

    feat: exception messeges beautify

--- src/main/java/com/milet/gestao_vagas/modules/candidate/entities/CandidateEntity.java
+++ src/main/java/com/milet/gestao_vagas/modules/candidate/entities/CandidateEntity.java
@@ -1,8 +1,12 @@
 package com.milet.gestao_vagas.modules.candidate.entities;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 import jakarta.validation.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+
 import java.util.UUID;
 
 @Data
@@ -10,10 +14,15 @@ public class CandidateEntity {
 
     private UUID id;
     private String name;
+
+    @NotBlank
+    @Pattern(regexp = "\\S+", message = "O campo [username] não pode conter espaço")
     private String username;
 
-    @Email
+    @Email(message = "O campo [e-mail] deve conter um e-mail válido")
     private String email;
+
+    @Length(min = 1, max = 20, message = "A senha deve conter entre 1 a 20 caracteres")
     private String password;
     private String description;
     private String curriculum;

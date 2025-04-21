5347d8efab324a847a74e658ee6fd9e205aaa748
milet <milet@milets-Mac-mini.local>
2025-04-21 16:12:20 -0300

    feat: entity job

---
+++ src/main/java/com/milet/gestao_vagas/modules/company/entities/CompanyEntity.java
@@ -0,0 +1,45 @@
+package com.milet.gestao_vagas.modules.company.entities;
+
+import com.milet.gestao_vagas.modules.company.entities.CompanyEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name = "company")
+public class CompanyEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank(message = "O campo [name] precisa ser preenchido")
+    private String name;
+
+    @NotBlank(message = "O campo [username] precisa ser preenchido")
+    @Pattern(regexp = "\\S+", message = "O campo [username] não pode conter espaço")
+    private String username;
+
+    @Email(message = "O campo [e-mail] deve conter um e-mail válido")
+    private String email;
+
+    @Length(min = 1, max = 20, message = "A senha deve conter entre 1 a 20 caracteres")
+    private String password;
+
+    private String website;
+
+    private String description;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}

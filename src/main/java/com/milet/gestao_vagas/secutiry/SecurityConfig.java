54fdfb2903e0b754f4e8d04455f90c390a9b1e70
milet <milet@milets-Mac-mini.local>
2025-05-02 16:39:07 -0300

    feat: jwt provider

--- src/main/java/com/milet/gestao_vagas/secutiry/SecurityConfig.java
+++ src/main/java/com/milet/gestao_vagas/secutiry/SecurityConfig.java
@@ -1,15 +1,20 @@
 package com.milet.gestao_vagas.secutiry;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private SecurityFilterConfig securityFilterConfig;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -19,7 +24,9 @@ public class SecurityConfig {
                             .requestMatchers("/company").permitAll()
                             .requestMatchers("/auth/company").permitAll()
                             .anyRequest().authenticated();
-                });
+                })
+                .addFilterBefore(securityFilterConfig, BasicAuthenticationFilter.class);
+
         return http.build();
     }
 

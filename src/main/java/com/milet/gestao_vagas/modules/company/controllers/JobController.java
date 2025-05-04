43748322f5b43b302b2531a0c102f87544ae4768
milet <milet@milets-Mac-mini.local>
2025-05-04 10:14:19 -0300

    feat: job controller companyID and CreateJobDTO

--- src/main/java/com/milet/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/com/milet/gestao_vagas/modules/company/controllers/JobController.java
@@ -1,13 +1,17 @@
 package com.milet.gestao_vagas.modules.company.controllers;
 
+import com.milet.gestao_vagas.modules.company.dtos.CreateJobDTO;
 import com.milet.gestao_vagas.modules.company.entities.JobEntity;
 import com.milet.gestao_vagas.modules.company.useCases.CreateJobUseCase;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/job")
@@ -17,7 +21,16 @@ public class JobController {
     private CreateJobUseCase createJobUseCase;
 
     @PostMapping("/")
-    public JobEntity create(@Valid @RequestBody JobEntity jobEntity){
+    public JobEntity create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request){
+        var companyId = request.getAttribute("company_id");
+
+        var jobEntity = JobEntity.builder()
+                .companyId(UUID.fromString(companyId.toString()))
+                .benefits(createJobDTO.getBenefits())
+                .description(createJobDTO.getDescription())
+                .level(createJobDTO.getLevel())
+                .build();
+
         return this.createJobUseCase.execute(jobEntity);
     }
 }
---
+++ src/main/java/com/milet/gestao_vagas/modules/company/dtos/CreateJobDTO.java
@@ -0,0 +1,12 @@
+package com.milet.gestao_vagas.modules.company.dtos;
+
+import lombok.Data;
+
+@Data
+public class CreateJobDTO {
+
+    private String description;
+    private String level;
+    private String benefits;
+
+}

e0ba56a4cf612bc7a8e00ebb8441b411aa69e63f
milet <milet@milets-Mac-mini.local>
2025-05-18 19:19:18 -0300

    feat: adding swagger

--- src/main/java/com/milet/gestao_vagas/GestaoVagasApplication.java
+++ src/main/java/com/milet/gestao_vagas/GestaoVagasApplication.java
@@ -1,13 +1,22 @@
 package com.milet.gestao_vagas;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Gestão de vagas",
+				description = "API responsável pela gestão de vagas",
+				version = "1.0"
+		)
+)
 public class GestaoVagasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestaoVagasApplication.class, args);
 	}
-
 }
--- src/main/java/com/milet/gestao_vagas/secutiry/SecurityConfig.java
+++ src/main/java/com/milet/gestao_vagas/secutiry/SecurityConfig.java
@@ -21,12 +21,20 @@ public class SecurityConfig {
     @Autowired
     private SecurityCandidateFilter securityCandidateFilter;
 
+    private static final String[] SWAGGER_LIST = {
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/swagger-resources/**"
+    };
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/candidate/**").permitAll()
+                    auth
+                            .requestMatchers("/candidate/**").permitAll()
                             .requestMatchers("/company/**").permitAll()
+                            .requestMatchers(SWAGGER_LIST).permitAll()
                             .anyRequest().authenticated();
                 })
                 .addFilterBefore(securityCandidateFilter, BasicAuthenticationFilter.class)

68856e9d643c8c7451aead64f11c39bfdab64b66
milet <milet@milets-Mac-mini.local>
2025-05-22 20:42:16 -0300

    feat: creatinf ApplyJobCandidateUseCase

---
+++ src/main/java/com/milet/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCase.java
@@ -0,0 +1,33 @@
+package com.milet.gestao_vagas.modules.candidate.useCases;
+
+import com.milet.gestao_vagas.exceptions.JobNotFoundException;
+import com.milet.gestao_vagas.exceptions.UserNotFoundException;
+import com.milet.gestao_vagas.modules.candidate.repositories.CandidateRepository;
+import com.milet.gestao_vagas.modules.company.repositories.JobRepository;
+import org.hibernate.validator.internal.constraintvalidators.bv.notempty.NotEmptyValidatorForArray;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class ApplyJobCandidateUseCase {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    public void execute(UUID idCandidate, UUID idJob){
+        this.candidateRepository.findById(idCandidate)
+                .orElseThrow(() -> {
+                   throw new UserNotFoundException();
+                });
+
+        this.jobRepository.findById(idJob)
+                .orElseThrow(() -> {
+                   throw new JobNotFoundException();
+                });
+    }
+}
